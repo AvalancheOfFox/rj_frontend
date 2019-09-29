0c71e113b390ab6b15c6beb955a920af88a62df4
Ross Perkel <rossperkel@gmail.com>
2019-09-29 15:28:28 -0400

    first css commits

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,10 +3,18 @@ import React, { Component } from 'react'
 
 export default class Question extends Component{
 
+
+    showMeDetails =() => {
+        console.log(this.props.category)
+    }
+
+
 render(){
     return(
-        <div>
+        <div className="single-question">
             <h4>{this.props.question.question}</h4>
+            <p>{this.props.category.title}</p>
+            <p>{this.showMeDetails()}</p>
         </div>
     )
 }
