17a888866e002cccc979c168af2277e16d792f6d
Ross Perkel <rossperkel@gmail.com>
2019-09-30 11:08:54 -0400

    got score working in state of gameboard

--- src/components/Question.js
+++ src/components/Question.js
@@ -10,11 +10,14 @@ export default class Question extends Component{
 
 render(){
     return(
-        <div className="single-question" onClick={ () => {this.setState({answered:true})}}>
+        <div className="single-question" >
+            <div onClick={() => this.props.handleQuestionClick(this.props.question)}>
             <h1>Value: ${this.props.question.value}</h1>
             <h4>{this.props.question.question}</h4>
             <p className={!this.state.answered == true ? "hidden": ""}>{this.props.question.answer}</p>
-            
+            </div>
+            <div className="correct" onClick={() => this.props.plusScore(this.props.question.value)}> Correct </div>
+            <div className="incorrect" onClick={() => this.props.minusScore(this.props.question.value)}> Incorrect </div>
         </div>
     )
 }
